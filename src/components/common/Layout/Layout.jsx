80498ca53ddc4cce26c464ad6d8f9fab9a0ccb99
ruben rodriguez <ruben.rodriguez.rur7@gmail.com>
2023-06-20 16:17:32 -0500

    new project

--- src/components/common/Layout/Layout.jsx
+++ src/components/common/Layout/Layout.jsx
@@ -15,11 +15,7 @@ const Layout = () => {
   return (
     <div>
       <header className="">
-        <div className="logo-header">
-          <img
-            src="https://s3-alpha-sig.figma.com/img/ca59/d9ce/98042af437fdff212d3259040db2e2db?Expires=1685923200&Signature=nMY1KszWu1~qq7kxGEJRphghbXXvBvk33QotdeoaHfrIMQ8SeDK~5lLdVz-3ujOHlZooSwxX8YPNCciZ3nX8lvIU4WCSLeeZj-h-KAYc~Ne33A3-rjiyRaMfRDzeLJ0XRN6YrkgNiiPHMb-Yu-p0d0h7nr1X7dBwUJKgy943Z~LUuGWS4tV7OkNz4Cf7BrIt2SVqPH00e8rTh44igGTaalgTSgKZU9XFe~DprWjxWc7owZcOYhJO9l88xicwoCjlAz4RytbcQhgBzrAUsBce0VkmAsH3q0XRDTjWCA5t7ed95QJUh0kw9QmQEogeQilFaiUxqVJtfk9VJC4cNYzJ9Q__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt="Pokedex"/>
-        </div>
-        <div className="logo-header_aux"></div>
+        <div className="logo-header"></div>
 
         <button className="logout_btn" onClick={logout}>Log out</button>
       </header>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -19,10 +19,7 @@ const Home = () => {
 
   return (
     <section>
-      <div className="logo-container">
-        <img
-          src="https://s3-alpha-sig.figma.com/img/ca59/d9ce/98042af437fdff212d3259040db2e2db?Expires=1685923200&Signature=nMY1KszWu1~qq7kxGEJRphghbXXvBvk33QotdeoaHfrIMQ8SeDK~5lLdVz-3ujOHlZooSwxX8YPNCciZ3nX8lvIU4WCSLeeZj-h-KAYc~Ne33A3-rjiyRaMfRDzeLJ0XRN6YrkgNiiPHMb-Yu-p0d0h7nr1X7dBwUJKgy943Z~LUuGWS4tV7OkNz4Cf7BrIt2SVqPH00e8rTh44igGTaalgTSgKZU9XFe~DprWjxWc7owZcOYhJO9l88xicwoCjlAz4RytbcQhgBzrAUsBce0VkmAsH3q0XRDTjWCA5t7ed95QJUh0kw9QmQEogeQilFaiUxqVJtfk9VJC4cNYzJ9Q__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt="Pokedex"/>
-      </div>
+      <div className="logo-container"></div>
 
       <h1 className="home_title">!Hola entrenador</h1>
       <p className="home_description">Para poder comenzar, dame tu nombre</p>
